29c3a8888dc97a355cede52605cf1d42053bd052
alinashev <al.t.shevchenko@gmail.com>
2022-01-13 14:05:54 +0200

    Add lambda-template

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
 def lambda_handler(event, context):
-	print("Test lambda_handler ")
-    return("Successfully completed")
+    for i in range(10):
+        print(i)
+    print("Test lambda_handler ")
+    return "Successfully completed"
